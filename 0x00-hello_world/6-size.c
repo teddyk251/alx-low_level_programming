67502a22a80c30cf375c2ecc74dab9393080917c
teddyk251 <teddylnk1@gmail.com>
2022-03-10 16:56:02 +0300

    fix typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return : size of various types
+ * Return: size of various types
  */
 
 int main(void)
@@ -13,10 +13,11 @@ int main(void)
 	float floatType;
 	long longInt;
 	long long llInt;
-	printf("Size of a char: %zu byte\n", sizeof(charType));
-	printf("Size of an int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes\n", sizeof(longInt));
-	printf("Size of a long long int: %zu bytes\n", sizeof(llInt));
-	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(llInt));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
