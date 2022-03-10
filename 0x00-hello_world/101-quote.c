3a50cf66c36a38b391001190faf6633fb045c990
teddyk251 <teddylnk1@gmail.com>
2022-03-10 17:47:09 +0300

    attempt 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, ch, 59);
 	return (1);
