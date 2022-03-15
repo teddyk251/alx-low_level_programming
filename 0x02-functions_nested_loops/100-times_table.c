c34c310f2398622453260a604c833f337467b4a9
teddyk251 <teddylnk1@gmail.com>
2022-03-15 18:25:19 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 table
- *
+ * print_times_table - prints the 9 table
+ * @n: n times table
  *  Description: prints all the nine table
  * Return: Always (0).
  */
 
 void print_times_table(int n)
 {
-	int i, j, product;
+    int i, j, product;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			if (j == 0)
-			{
-				_putchar(48);
-				continue;
-			}
-			product = i * j;
-			_putchar(',');
-			_putchar(' ');
-			if (product >= 10)
-			{
-				_putchar(product / 10 + 48);
-				_putchar(product % 10 + 48);
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(product + 48);
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (j == 0)
+            {
+                _putchar(48);
+                continue;
+            }
+            product = i * j;
+            _putchar(',');
+            _putchar(' ');
+            if (product >= 10)
+            {
+                _putchar(product / 10 + 48);
+                _putchar(product % 10 + 48);
+            }
+            else
+            {
+                _putchar(' ');
+                _putchar(product + 48);
+            }
+        }
+        _putchar('\n');
+    }
 }
