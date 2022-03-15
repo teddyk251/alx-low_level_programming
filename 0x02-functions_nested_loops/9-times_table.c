44b3a874d0c84b2461a8ac454c8de5565d8ad79b
teddyk251 <teddylnk1@gmail.com>
2022-03-15 17:51:42 +0300

    question 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 table
+ *
+ *  Description: prints all the nine table
+ * Return: Always (0).
+ */
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			product = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (product >= 10)
+			{
+				_putchar(product / 10 + 48);
+				_putchar(product % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(product + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
