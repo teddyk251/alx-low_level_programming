85d1d69b79f05c8e7568a00a0b8c11d5fb42e161
teddyk251 <teddylnk1@gmail.com>
2022-03-16 16:05:21 +0300

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,16 +11,12 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-
 	if (n >= 0 && n <= 15)
 	{
-
 		for (i = 0; i <= n; i++)
 		{
-
 			for (j = 0; j <= n; j++)
 			{
-
 				if ((i * j) < 10)
 				{
 					if (j != 0)
@@ -31,7 +27,6 @@ void print_times_table(int n)
 					}
 					_putchar((j * i) + '0');
 				}
-
 				else if ((i * j) < 100)
 				{
 					_putchar(' ');
@@ -39,7 +34,6 @@ void print_times_table(int n)
 					_putchar((((j * i) / 10) % 10) + '0');
 					_putchar(((j * i) % 10) + '0');
 				}
-
 				else if ((i * j) < 1000)
 				{
 					_putchar(' ');
@@ -51,13 +45,8 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 			}
-
 			}
-
 		_putchar('\n');
-
 		}
-
 	}
-
 }
