5a5df0d5946861d201448077fbc36efc9e7491ba
teddyk251 <teddylnk1@gmail.com>
2022-03-17 20:52:44 +0300

    advanced 2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *print_number - prints an integer
+ *
+ *@n:input
+ *
+ *Return:void
+ *
+ *Description:prints an integer
+ *
+ */
+void print_number(int n)
+{
+	unsigned int x, y, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	else
+	{
+		x = n;
+	}
+	y = x;
+	count = 1;
+
+	while (y > 9)
+	{
+		y /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((x / count) % 10) + 48);
+	}
+}
