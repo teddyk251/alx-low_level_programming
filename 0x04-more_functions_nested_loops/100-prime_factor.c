cb8688239fed1f31d6be210b5804e52e177d942e
teddyk251 <teddylnk1@gmail.com>
2022-03-17 20:51:23 +0300

    fix logic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,20 +7,29 @@
  */
 int main(void)
 {
+	long int n, max, i;
 
-unsigned int long n = 612852475143, a = (int) sqrt(n);
+	max = -1;
+	n = 612852475143;
 
-while (1)
-{
-
-	if (n % a == 0)
+	while (n % 2 == 0)
 	{
-		printf("%lu \n", n / a);
-		break;
+		max = 2;
+		n = n / 2;
 	}
-	a--;
-
+	for (i = 3; i <= n / 2; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%ld\n", max);
+	return (0);
 }
 
-return (0);
-}
