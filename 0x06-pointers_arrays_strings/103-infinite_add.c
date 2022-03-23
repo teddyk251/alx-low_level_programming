9b5089772af90c833bff307caa3d66fc9485dd7d
teddyk251 <teddylnk1@gmail.com>
2022-03-23 18:43:44 +0300

    bug fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,74 +1,101 @@
 #include "main.h"
-
-
 /**
- * _atoi - add 2 integers.
- * @s: char
- * Return: int
+ *rev_string - reverses a string
+ *
+ *@n:string input
+ *
+ *Return:void
+ *
  */
-int _atoi(char *s)
+void rev_string(char *n)
 {
-int sign = 1, resp = 0, firstNum;
+	int i = 0, j = 0;
+	char temp;
 
-for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-{
-if (s[firstNum] == '-')
-{
-sign *= -1;
-}
-}
-
-for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-{
-resp *= 10;
-resp += (s[i] - 48);
-}
-
-return (sign *resp);
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 /**
- * int_to_string - add 2 integers.
- * @n: integer
- * Return: 0
- */
-
-void int_to_string(int n)
-{
-int divisor = 1, i, resp;
-
-
-for (i = 0; n / divisor > 9; i++)
-{
-divisor *= 10;
-}
-
-char str[i];
-
-for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-{
-resp = n / divisor;
-str[cmpt] = '0' + resp;
-n = n - resp *divisor;
-}
-str[i] = ('0' + n);
-
-}
-
-/**
- * infinite_add - add 2 integers.
- * @n1: integer
- * @n2: integer
- * @r: buffer
- * @size_r: size of r
- * Return: char
+ *infinite_add - adds two long numbers,stores them.in a buffer
+ *
+ *@n1:one of the two numbers
+ *@n2:2nd no.
+ *@r:buffer to store result
+ *@size_r:buffer size
+ *
+ *Return:pointer to result
+ *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int sum, a, b;
-a = _atoi(n1);
-b = _atoi(n2);
+	int overflow = 0;
+	int i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-sum = a + b;
-
-
+	while (*(n1 + i) != '\0')
+	{
+		i++;
+	}
+	while (*(n2 + j) != '\0')
+	{
+		j++;
+	}
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+	{
+		return (0);
+	}
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+		{
+			val1 = 0;
+		}
+		else
+		{
+			val1 = *(n1 + i) - '0';
+		}
+		if (j < 0)
+		{
+			val2 = 0;
+		}
+		else
+		{
+			val2 = *(n2 + j) - '0';
+		}
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+		{
+			overflow = 1;
+		}
+		else
+		{
+			overflow = 0;
+		}
+		if (digits >= (size_r - 1))
+		{
+			return (0);
+		}
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+	{
+		return (0);
+	}
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
