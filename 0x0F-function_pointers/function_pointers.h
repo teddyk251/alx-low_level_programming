698e77997ffd11cead521e467f412655286de8d8
Teddy <teddylnk1@gmail.com>
2022-04-13 20:25:17 +0300

    add

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#ifndef FILE_F_POINTRS
+#define FILE_F_POINTRS
 
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* #ifndef FUNCTION_POINTERS_ */
+#endif
