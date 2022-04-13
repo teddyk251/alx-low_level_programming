7b44999ea4887449c9b3bc5c64024bc4bccd6a6d
Teddy <teddylnk1@gmail.com>
2022-04-13 20:28:45 +0300

    advanced 1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,36 +1,39 @@
-
-  
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *main - prints its opcodes
+ * main - Prints the opcodes of this function
  *
- *@argc:arguement count
- *@argv:arguement vector
+ * @argc: The number of command-line arguments
  *
- *Return:int
+ * @argv: The command-line arguments
  *
+ * Return: 0 if successful
  */
+
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *p = (char *)main;
+	int n_bytes, i;
+	int (*prog)(int, char **) = &main;
 
-	if (argc != 2)
+	if (argc == 2)
+	{
+		n_bytes = atoi(argv[1]);
+		if (n_bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		for (i = 0; i < n_bytes; i++)
+			printf("%02x%c",
+					(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
+	}
+	else
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	for (i = 0; i < bytes - 1; i++)
-	{
-		printf("%02hhx ", p[i]);
-	}
-	printf("%02hhx\n", p[i]);
+
 	return (0);
+
 }
