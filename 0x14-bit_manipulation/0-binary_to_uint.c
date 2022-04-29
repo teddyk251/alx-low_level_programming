252c4020e1a09daf33ca5b118b6669bbce7448a0
Teddy <teddylnk1@gmail.com>
2022-04-29 10:04:20 +0300

    bug fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,48 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
-
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary number
- *
- * Return: 0 or converted number
+ *binary_to_uint - converts a binary number to an unsigned int
+ *@b:pointer to a string containing 1 and 0
+ *Return:converted number or 0,otherwise
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int n = 0;
-	int i, len;
 
 	if (b == NULL)
 		return (0);
-
-	len = _strlen(b);
-
-	for (i = 0; i != len; i++)
+	i = 0;
+	while (b[i])
 	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		i++;
 	}
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n = n << 1;
+		if (b[i] == '1')
+		{
+			n += 1;
+		}
+	}
 	return (n);
 }
